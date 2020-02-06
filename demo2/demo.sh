70aba5aa059fac4d4249fd5b5f60c12b9f415895
clark010 <zhanghao.pris@163.com>
2020-02-06 19:35:22 +0800

    add new modify

--- demo2/demo.sh
+++ demo2/demo.sh
@@ -1,4 +1,4 @@
-g++ -I./include -L./cmake-build-debug/src/ -lmainrepo -o main2 main2.cpp
+g++ -I./include -L./cmake-build-debug/src/ -ltestso -o main2 main2.cpp
 
 export LD_LIBRARY_PATH=./cmake-build-debug/src
 ./main2
